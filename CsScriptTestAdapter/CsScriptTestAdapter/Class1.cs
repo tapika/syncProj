c41b79b0b0a0832c4e8f84e32775e4bbc9ada43c
Tarmo Pikaro <tapika@yahoo.com>
2019-03-14 23:45:41 +0200

    Tune: Initial prototype for unit test / discovery test component

---
+++ CsScriptTestAdapter/CsScriptTestAdapter/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsScriptTestAdapter
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void RunAllTests()
+        {
+        }
+    }
+}
